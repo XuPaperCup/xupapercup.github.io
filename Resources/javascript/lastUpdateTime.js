e88eb05204e93a57ba3761c7eba36af19457aed2
XuPaperCup <32026718+XuPaperCup@users.noreply.github.com>
2022-06-02 22:18:22 +0800

    try

--- Resources/javascript/lastUpdateTime.js
+++ Resources/javascript/lastUpdateTime.js
@@ -2,7 +2,6 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     let repos = JSON.parse(this.responseText);
-    document.writeln("This Website Last Update Date:");
     repos.forEach((repo)=>{
       document.writeln(`<code>${repo.name}</code>: <em>${new Date(repo.updated_at)}</em><br>`);
     });
