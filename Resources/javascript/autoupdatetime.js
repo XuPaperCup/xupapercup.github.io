fe09701c659b86e2ae7c86a3aa0038cf9eaf3f17
XuPaperCup <32026718+XuPaperCup@users.noreply.github.com>
2022-06-02 20:11:04 +0800

    push

---
+++ Resources/javascript/autoupdatetime.js
@@ -0,0 +1,24 @@
+const desiredRepo = "xupapercup.github.io";
+const dateTagClass = ".autoupdatetime";
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function()
+{
+  if (this.readyState == 4 && this.status == 200)
+  {
+    let repos = JSON.parse(this.responseText);
+
+    repos.forEach((repo)=>{
+      if (repo.name == desiredRepo)
+      {
+        var lastUpdated = new Date(repo.updated_at);
+        var day = lastUpdated.getUTCDate();
+        var month = lastUpdated.getUTCMonth();
+        var year = lastUpdated.getUTCFullYear();
+        $(dateTagClass).text(`Last updated: ${year}-${month}-${day}`);
+      }
+    });
+  }
+};
+xhttp.open("GET", "https://api.github.com/users/xupapercup/repos", true);
+xhttp.send();
