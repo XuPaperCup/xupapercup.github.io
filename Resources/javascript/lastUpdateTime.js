e759f2bcd292c47602b4cf563702c6a374cbdeac
XuPaperCup <32026718+XuPaperCup@users.noreply.github.com>
2022-06-02 21:47:54 +0800

    push

--- Resources/javascript/lastUpdateTime.js
+++ Resources/javascript/lastUpdateTime.js
@@ -2,8 +2,9 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     let repos = JSON.parse(this.responseText);
+    document.write("This Website Last Update Date:");
     repos.forEach((repo)=>{
-      document.getElementById('lastUpdateTime').innerHTML += `<code>${repo.xupapercup.github.io}</code>: <em>${new Date(repo.updated_at)}</em><br>`;
+      document.writeln(`<code>${repo.xupapercup.github.io}</code>: <em>${new Date(repo.updated_at)}</em><br>`);
     });
   }
 };
