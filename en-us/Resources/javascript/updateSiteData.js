1fd0f9faf465295b3d5e2713b0bfde5ae3f240d2
XuPaperCup <32026718+XuPaperCup@users.noreply.github.com>
2022-06-06 20:53:47 +0800

    Update updateSiteData.js

--- en-us/Resources/javascript/updateSiteData.js
+++ en-us/Resources/javascript/updateSiteData.js
@@ -5,7 +5,7 @@ const upcoming_change_data = "Upcoming Change: zh-hant/404.html (0%)";
 
 // Get a reference to the element we want to update
 current_status = document.getElementById("current_status");
-recent_update = document.getElementById("next_update");
+recent_update = document.getElementById("recent_update");
 upcoming_change = document.getElementById("upcoming_change");
 
 // Update the content of the element with the message
