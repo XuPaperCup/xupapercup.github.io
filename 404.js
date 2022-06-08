64b6ee4e20b4237b02138d2839d1bff50a9eb004
XuPaperCup <32026718+XuPaperCup@users.noreply.github.com>
2022-06-08 23:49:56 +0800

    Update 404.js

--- 404.js
+++ 404.js
@@ -157,7 +157,7 @@ function display_404_in_en_us() {
     let body_line4_data = "Click Go Back or Welcome Page to exit this error page";
     // Preset Constant Word - Data
     let data_line1_data = "Error type: Web Page Not found";
-    let data_line2_data = 'Error code: <a href="https://zh.wikipedia.org/zh-tw/HTTP_404" target="_blank" id="powered">HTML 404</a>';
+    let data_line2_data = 'Error code: <a href="https://zh.wikipedia.org/zh-tw/HTTP_404" target="_blank" id="powered">HTTP 404</a>';
     let data_line3_data = "Display Language: English";
     let data_line4_data = '<div id="lastUpdateTime">Website Last Update Time (Local Time): Getting GitHub API Data...</div>';
     let data_line5_data = 'XuPaperCup&apos;s Website<BR><a href="https://pages.github.com/" target="_blank" id="powered">Powered by <b>GitHub</b> Pages</a>';
@@ -210,7 +210,7 @@ function display_404_in_zh_hant() {
     let body_line4_data = "單擊返回上一頁或回到首頁來離開此錯誤網頁";
     // Preset Constant Word - Data
     let data_line1_data = "錯誤類型: 找不到網頁";
-    let data_line2_data = '錯誤代碼: <a href="https://zh.wikipedia.org/zh-tw/HTTP_404" target="_blank" id="powered" >HTML 404</a>';
+    let data_line2_data = '錯誤代碼: <a href="https://zh.wikipedia.org/zh-tw/HTTP_404" target="_blank" id="powered" >HTTP 404</a>';
     let data_line3_data = "顯示語言: 繁體中文";
     let data_line4_data = '<div id="lastUpdateTime">網站最後更新時間(本地時間): 讀取GitHub API數據中...</div>';
     let data_line5_data = 'XuPaperCup的網站<BR><a href="https://pages.github.com/" target="_blank" id="powered" style="float: none; text-align: center;">由&thinsp;<b>GitHub</b>&thinsp;Pages&thinsp;提供支持</a>';
