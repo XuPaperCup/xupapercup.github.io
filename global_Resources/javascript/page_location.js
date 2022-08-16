c5667732ffa70fcd516d481148e593d13358ef42
XuPaperCup <32026718+XuPaperCup@users.noreply.github.com>
2022-08-17 05:27:24 +0800

    move folder

--- global_Resources/javascript/page_location.js
+++ global_Resources/javascript/page_location.js
@@ -1,9 +1,7 @@
 // Preset Constant Function
 let page_location_data = window.location
-
 // Get a reference to the element we want to update
 var page_location = document.getElementById("page_location");
-
 // Update the content of the element with the message
 if (language == "en-us") {
     page_location.innerHTML = "Requested Page Directory: " + page_location_data;
@@ -12,15 +10,12 @@ if (language == "zh-hant") {
     page_location.innerHTML = "請求的頁面目錄: " + page_location_data;
 }
 
-
 // Execute in function
 function page_location_function() {
 // Preset Constant Function
 let page_location_data = window.location
-
 // Get a reference to the element we want to update
 var page_location = document.getElementById("page_location");
-
 // Update the content of the element with the message
 if (language == "en-us") {
     page_location.innerHTML = "Requested Page Directory: " + page_location_data;
