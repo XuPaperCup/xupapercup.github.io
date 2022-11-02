9462595c74a50e318dd693b558879e0d0654ae83
XuPaperCup <32026718+XuPaperCup@users.noreply.github.com>
2022-11-02 13:20:14 +0800

    Bottom Frame Optimization Phrase 4
    Small changes in [Update Page]

--- global_Resources/javascript/module/primary/showBttmFrame.js
+++ global_Resources/javascript/module/primary/showBttmFrame.js
@@ -33,5 +33,27 @@ async function showBttmFrame() {
             bottom_frame.innerHTML = bottom_line;
         }
     }
+    if (language === "zh-hant") {
+        let bottom_frame = document.getElementById("bottom_frame");
+            if (bottom_frame === null) {
+                console.log(`\nUnable to execute Module [showBttmFrame] await [lastUpdateTime],\nReason: There is no "bottom_frame" identifier in this web page element.`);
+            }
+            else {
+                let updateTimeMsg;
+            try {
+                updateTimeMsg = await lastUpdateTime(); 
+            } catch (e) {
+                console.log(e);
+                updateTimeMsg = `<FONT color="red">錯誤! ${e}</FONT>`;
+            }
+            const bottom_line = `<HR>\n
+            <FONT size="5">Current Language: English</FONT><BR>
+            <FONT size="5"><div id="lastUpdateTime">${updateTimeMsg}</div></FONT>
+            <FONT size="5" style="background-color: gray; color: white">Got a problem or suggestion? Tell them in Home Page➡Miscellaneous➡Report Website Issue(s)!</FONT><BR>
+            <FONT size="5" color="#AFAFAF" style="float: right; text-align: right;">XuPaperCup's Website<BR>
+            <a href="https://github.com/XuPaperCup/xupapercup.github.io" target="_blank" id="powered">Build in <b>GitHub</b></a></FONT>`;
+            bottom_frame.innerHTML = bottom_line;
+        }
+    }
 }
 export { showBttmFrame };
