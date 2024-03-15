f7de3b8a248fd2cc98057a4501a752795d6e60f3
XuPaperCup <32026718+XuPaperCup@users.noreply.github.com>
2024-03-16 04:23:05 +0800

    update

--- Resources/javascript/.OLD/entry.js
+++ Resources/javascript/.OLD/entry.js
@@ -2,8 +2,8 @@
 import { showBttmFrame } from './module/primary/showBttmFrame.js';
 import { showPageLoc } from './module/primary/showPageLoc.js';
 
-function main() {
-showBttmFrame();
-showPageLoc();
+function entry() {
+    showBttmFrame();
+    showPageLoc();
 }
-export { main };
+export { entry };
